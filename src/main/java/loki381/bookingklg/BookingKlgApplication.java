8731ac1fba68848a4d212135fa119b150233d6d9
kamolsz2 <kamil.olszewski@pg.edu.pl>
2023-03-09 15:26:33 +0100

    init project

---
+++ src/main/java/loki381/bookingklg/BookingKlgApplication.java
@@ -0,0 +1,13 @@
+package loki381.bookingklg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingKlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingKlgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loki381/bookingklg/BookingKlgApplicationTests.java
@@ -0,0 +1,13 @@
+package loki381.bookingklg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingKlgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
