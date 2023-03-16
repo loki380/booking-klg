f025bc0249f30521f9480c0b5dac3d6c0cc5d9bc
loki380 <kamil.olszewski35@gmail.com>
2023-03-16 19:34:04 +0100

    Update postman export

--- src/main/java/loki381/bookingklg/model/LandlordReport.java
+++ src/main/java/loki381/bookingklg/model/LandlordReport.java
@@ -1,5 +1,6 @@
 package loki381.bookingklg.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -11,8 +12,12 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class LandlordReport {
 
+    @Schema(description = "Id wynajmującego")
     private Long landlordId;
+    @Schema(description = "Nazwa wynajmującego")
     private String landlordName;
+    @Schema(description = "Liczba rezerwacji")
     private Integer bookings;
+    @Schema(description = "Zyski")
     private BigDecimal profits;
 }
